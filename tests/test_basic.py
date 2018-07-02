80eb602c3c58fa972ece42ba79627d615fb6fcdc
Evan Ciancio <eciancio@llnw.com>
2018-07-02 11:56:04 -0400

    Hardcode in database creds for test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,17 +3,15 @@ import unittest
 import mysql.connector
 import sys
 from os import path
-sys.path.append("../" )
-import constants
 
 
 class TestDataBaseConnection(unittest.TestCase):
     def test_connect(self):
          try:
-            cnx = mysql.connector.connect(user=constants.testUser,
-                host=constants.testHost,
-                database=constants.testName,
-                password=constants.testPassword)                                                                                                               
+            cnx = mysql.connector.connect(user="root",
+                host="127.0.0.1",
+                database="mlb",
+                password="")                                                                                                               
             cusror = cnx.cursor()
             self.assertEqual(1, 1)
          except Exception as e:
